b871c291856f8356263fca3af92e8828e41eb753
Aaron Hope <aghopecode@gmail.com>
2022-06-07 01:23:37 -0700

    Added getDocs() in /src/store/index.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app"
+import {
+  getFirestore
+} from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBV6tP6MQxXBuX-HCMoty-vzYiZ3lWhnhg",
@@ -9,4 +12,11 @@ const firebaseConfig = {
   appId: "1:520474884624:web:190538b66590de64f8a895"
 };
 
+
+// init firebase app
 initializeApp(firebaseConfig)
+
+// init firestore services
+const db = getFirestore()
+
+export { db }
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,45 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+import { db } from '../firebase/config'
+
+import { 
+  collection,
+  getDocs
+} from "firebase/firestore"
+
+// collection reference
+const colRef = collection(db, 'books')
+
+// get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) => {
+      // so doc.data() gets the data properties EXCEPT the id
+      books.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+
+const store = createStore({})
+
+
+export default store
+
+
+
+// export default createStore({
+//   state: {
+//   },
+//   getters: {
+//   },
+//   mutations: {
+//   },
+//   actions: {
+//   },
+//   modules: {
+//   }
+// })
