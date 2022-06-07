24e42fb0fb1292817d0d5e19363ec05cbe54479f
Aaron Hope <aghopecode@gmail.com>
2022-06-07 00:38:58 -0700

    Created /src/firebase/config.js

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBV6tP6MQxXBuX-HCMoty-vzYiZ3lWhnhg",
+  authDomain: "getting-started-firebase-9.firebaseapp.com",
+  projectId: "getting-started-firebase-9",
+  storageBucket: "getting-started-firebase-9.appspot.com",
+  messagingSenderId: "520474884624",
+  appId: "1:520474884624:web:190538b66590de64f8a895"
+};
+
+initializeApp(firebaseConfig)
